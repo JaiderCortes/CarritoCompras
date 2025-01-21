89161e3903219d884c6864c69ce93d4f187a2ac2
JaiderCortes <jacortessa@gmail.com>
2025-01-20 21:52:26 -0500

    Avances en creación de capas, módulos, diseño, vistas y controladores.

---
+++ CapaDatos/CD_Usuarios.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaEntidad;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CapaDatos
+{
+    public class CD_Usuarios
+    {
+        public List<Usuario> Listar()
+        {
+            List<Usuario> usuarios = new List<Usuario>();
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(Conexion.con))
+                {
+                    con.Open();
+                    string sql = "SELECT IdUsuario, Nombres, Apellidos, Correo, Clave, Reestablecer, Activo FROM Usuario;";
+                    SqlCommand cmd = new SqlCommand(sql, con);
+                    cmd.CommandType = CommandType.Text;
+                    using (SqlDataReader rst = cmd.ExecuteReader())
+                    {
+                        while (rst.Read())
+                        {
+                            Usuario usuario = new Usuario()
+                            {
+                                IdUsuario = Convert.ToInt32(rst["IdUsuario"]),
+                                Nombres = Convert.ToString(rst["Nombres"]),
+                                Apellidos = Convert.ToString(rst["Apellidos"]),
+                                Correo = Convert.ToString(rst["Correo"]),
+                                Clave = Convert.ToString(rst["Clave"]),
+                                Reestablecer = Convert.ToBoolean(rst["Reestablecer"]),
+                                Activo = Convert.ToBoolean(rst["Activo"])
+                            };
+                            usuarios.Add(usuario);
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                usuarios = new List<Usuario>();
+            }
+
+            return usuarios;
+        }
+    }
+}
--- CapaPresentacionAdmin/Controllers/HomeController.cs
+++ CapaPresentacionAdmin/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CapaEntidad;
+using CapaNegocio;
 
 namespace CapaPresentacionAdmin.Controllers
 {
@@ -13,18 +15,17 @@ namespace CapaPresentacionAdmin.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Usuarios()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
+        [HttpGet]
+        public JsonResult ListarUsuarios()
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            List<Usuario> usuarios = new List<Usuario>();
+            usuarios = new CN_Usuarios().Listar();
+            return Json(new { data = usuarios }, JsonRequestBehavior.AllowGet);
         }
     }
 }
